426add3d59c73c84c196a780471650966de907fa
Shinner <skyggendk@gmail.com>
2022-11-01 15:44:59 +0100

    Added buttons for the submenu under items and create. Create now defaults to the create item view. Items button defaults to viewing standard items.

---
+++ AubreysPeculiarities/MVVM/View/CreateItemView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AubreysPeculiarities.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for CreateView.xaml
+    /// </summary>
+    public partial class CreateItemView : UserControl
+    {
+        public CreateItemView()
+        {
+            InitializeComponent();
+        }
+    }
+}
